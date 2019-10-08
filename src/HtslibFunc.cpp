957b3a2d8046adc6e163ba5b6d2193431ab107f0
ZhangchenZhao <zczhao@umich.edu>
2019-10-08 12:16:49 -0400

    Update HtslibFunc.cpp

--- src/HtslibFunc.cpp
+++ src/HtslibFunc.cpp
@@ -8,11 +8,11 @@
  #include <limits>
 #include "zlib.h"
 #include "zconf.h"
-#include "hts.h"
-#include "vcf.h"
-#include "kstring.h"
-#include "kseq.h"
-#include "khash_str2int.h"
+#include "htslib/hts.h"
+#include "htslib/vcf.h"
+#include "htslib/kstring.h"
+#include "htslib/kseq.h"
+#include "htslib/khash_str2int.h"
 #include "PlinkBed.h"
 #include <algorithm>
 #include <sstream>
